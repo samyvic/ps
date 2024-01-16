4401b522bcfcbb92365672bc641119f2ee6b9149
Ahmed Samy <ahmedov.samyvic@yahoo.com>
2024-01-16 22:54:19 +0200

    Exercising reduce function

---
+++ reduce.js
@@ -0,0 +1,19 @@
+const arr = [5,1,3,2,6,10];
+
+const sum = (number, output=0) => {
+    return output += number;
+}
+const max = (number, maximum=0) => {
+    return maximum = number > maximum ? number : maximum;
+}
+const average = () => {
+    return arr.reduce(sum)/arr.length;
+}
+
+const operation = (logic) =>{
+    return arr.reduce(logic);
+}
+
+console.log(operation(sum))
+console.log(operation(max))
+console.log(operation(average))
